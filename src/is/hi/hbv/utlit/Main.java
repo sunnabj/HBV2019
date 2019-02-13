a77e5d2c4c9871eadc0bffb9b72b5349999fe091
Hieu Van Phan <hieutote@yahoo.com>
2019-02-13 18:01:58 +0000

    breyta workspace
    og main laga glugga

--- src/is/hi/hbv/utlit/Main.java
+++ src/is/hi/hbv/utlit/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Search.fxml"));
         primaryStage.setTitle("Hótel Íslands");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1150, 750));
         primaryStage.show();
     }
 
